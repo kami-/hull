9fe0f2f1be494b39200d3f9a6e54392bc15b6083
László Major <laszlo.major@outlook.com>
2014-07-08 19:10:22 +0200

    Reorganized header files.

---
+++ hull/hull.h
@@ -0,0 +1,24 @@
+#include "hull_xeh.h"
+
+class Hull {
+    #include "mission_params.h"
+
+    class Gear {
+        #include "gear\factions.h"
+
+        #include "gear\AK103.h"
+        #include "gear\AK74.h"
+        #include "gear\AK74M.h"
+        #include "gear\AK74P.h"
+        #include "gear\AKM.h"
+        #include "gear\HK416.h"
+        #include "gear\L85.h"
+        #include "gear\M16A2.h"
+        #include "gear\M16A4.h"
+        #include "gear\M4SOC.h"
+        #include "gear\SCAR.h"
+        #include "gear\XM8.h"
+    };
+};
+
+
